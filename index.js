e7ab66a6eae6111200f28bd94722092c382e017c
sahil <sahil27raj@gmail.com>
2024-04-28 13:13:34 +0530

    add the solution for question number 8

---
+++ index.js
@@ -0,0 +1,14 @@
+// Q.Write a function called removeDuplicates that takes an array of numbers as
+//  input and returns a new array with only the unique elements (i.e., removing any duplicate elements).
+
+// removeDuplicates([1, 2, 3, 4, 5]); // Expected output: [1, 2, 3, 4, 5]
+// removeDuplicates([1, 2, 2, 3, 3, 4, 5]); // Expected output: [1, 2, 3, 4, 5]
+// removeDuplicates([1, 1, 1, 1, 1]); // Expected output: [1]
+// removeDuplicates([]); // Expected output: []
+
+
+
+const removeDuplicates =(array)=>{
+    return array.filter((value,i)=> array.indexOf(value) === i)
+}
+console.log(removeDuplicates([1, 1, 1, 1, 1]));
---
+++ readme.txt
@@ -0,0 +1,7 @@
+// Q.Write a function called removeDuplicates that takes an array of numbers as
+//  input and returns a new array with only the unique elements (i.e., removing any duplicate elements).
+
+// removeDuplicates([1, 2, 3, 4, 5]); // Expected output: [1, 2, 3, 4, 5]
+// removeDuplicates([1, 2, 2, 3, 3, 4, 5]); // Expected output: [1, 2, 3, 4, 5]
+// removeDuplicates([1, 1, 1, 1, 1]); // Expected output: [1]
+// removeDuplicates([]); // Expected output: []
